0cd328abe3fad24ce37252724ef69b5e4ddc44f5
MaglorFeanorson <daniilrog17@gmail.com>
2024-08-04 13:28:39 +0300

    "fix: fixed flake8 issues"

--- cinema/models.py
+++ cinema/models.py
@@ -1,19 +1,28 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+
+
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
+
     @property
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
+
     def __str__(self):
         return self.name
+
+
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
     def __str__(self):
         return self.name
+
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -25,24 +34,33 @@ class Actor(models.Model):
     def __str__(self):
         return self.first_name + " " + self.last_name
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+
     class Meta:
         ordering = ["title"]
+
     def __str__(self):
         return self.title
+
+
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+
     class Meta:
         ordering = ["-show_time"]
+
     def __str__(self):
         return self.movie.title + " " + str(self.show_time)
+
+
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
@@ -52,8 +70,10 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.created_at)
+
     class Meta:
         ordering = ["-created_at"]
+
 
 class Ticket(models.Model):
     movie_session = models.ForeignKey(
@@ -68,6 +88,7 @@ class Ticket(models.Model):
     )
     row = models.IntegerField()
     seat = models.IntegerField()
+
     def clean(self):
         for ticket_attr_value, ticket_attr_name, cinema_hall_attr_name in [
             (self.row, "row", "count_rows"),
